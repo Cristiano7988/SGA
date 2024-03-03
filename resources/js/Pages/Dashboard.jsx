916ac5b7152afeb2b6354f108f7a739731caada8
cristianooliveiramorales@gmail.com <cristianooliveiramorales@gmail.com>
2024-03-03 10:26:09 -0300

    Customizando a página de dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,14 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { Chart } from 'react-google-charts';
 
 export default function Dashboard(props) {
+
+    const convites = [
+        ['Eventos', 'Total'],
+        ['Criados', props.eventos],
+        ['Comparecidos', props.comparecidos]
+    ]
     return (
         <AuthenticatedLayout
             auth={props.auth}
@@ -13,7 +20,16 @@ export default function Dashboard(props) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <div className="p-6 text-gray-900">
+                            {props.eventos || props.eventos_que_compareci ? <Chart
+                                data={convites}
+                                chartType='PieChart'
+                                options={{
+                                    title: 'Eventos',
+                                    is3D: true
+                                }}
+                            /> : <p>Não há registros de eventos</p>}
+                        </div>
                     </div>
                 </div>
             </div>
